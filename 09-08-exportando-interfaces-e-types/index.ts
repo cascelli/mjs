e195ce4e12d104bdb656c2ced0ac9934fbba4b94
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2021-07-07 20:06:07 -0300

    09-08-Exportando intrfaces e types

---
+++ 09-08-exportando-interfaces-e-types/index.ts
@@ -0,0 +1,10 @@
+// import { Person, Email } from "./interfaces";
+//
+const person: Person.WithContacts = {
+  name: 'Lucas',
+  age: 32,
+  contacts: []
+}
+
+//const email: Email = 'daniel@gmail.com'
+
