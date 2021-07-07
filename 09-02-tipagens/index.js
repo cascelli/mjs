b0d25a4874a374a638d8ac57e54e83b13666e256
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2021-07-07 12:23:49 -0300

    09-02-Tipagens

---
+++ 09-02-tipagens/index.js
@@ -0,0 +1,15 @@
+"use strict";
+// Codigo JavaScript
+//const personName = 'Daniel'
+// Codigo TypeScript, especificando o tipo da variável :
+// Tipo string
+const personName = 'Daniel';
+// Tipo number
+const personAge = 28;
+// Tipo boolean
+const personAlive = true;
+// Combinando tipos em arrays :
+// Array só de strings
+const contacts = ['Daniel', 'Marcos'];
+// array de strings ou numeros
+const personContacts = ['Daniel', 22, 'Marcos', 35];
---
+++ 09-02-tipagens/index.ts
@@ -0,0 +1,23 @@
+
+// Codigo JavaScript
+//const personName = 'Daniel'
+
+// Codigo TypeScript, especificando o tipo da variável :
+
+// Tipo string
+const personName:string = 'Daniel'
+
+// Tipo number
+const personAge:number = 28
+
+// Tipo boolean
+const personAlive:boolean = true
+
+// Combinando tipos em arrays :
+
+// Array só de strings
+const contacts:string[] = ['Daniel', 'Marcos'] 
+
+// array de strings ou numeros
+const personContacts:(string | number)[] = [ 'Daniel', 22, 'Marcos', 35]
+
